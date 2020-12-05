9459ed2c7581778273ed1e5142061ee638f34a55
Alex Huang <alex_huang@wistron.com>
2020-12-06 01:41:26 +0800

    Step 1

---
+++ index.js
@@ -0,0 +1,59 @@
+//
+// 自动化截取刷卡资料
+//
+
+'use strict';	// Whole-script strict mode applied.
+
+const http = require('http');   // NOTE: import default module
+const fs = require('fs');       // NOTE: import default module
+
+//
+// Step 1: Open login page to get cookie 'ASP.NET_SessionId' and hidden input '_ASPNetRecycleSession'.
+//
+var _ASPNET_SessionId;
+var _ASPNetRecycleSession;
+
+function step1() {
+
+    function callback(response) {
+        let chunks = [];
+        response.addListener('data', (chunk) => {
+            chunks.push(chunk);
+        });
+        response.on('end', () => {
+            let buff = Buffer.concat(chunks);
+            let html = buff.toString();
+            if (response.statusCode===200) {
+                let fo = fs.createWriteStream('tmp/step1.html');
+                fo.write(html);
+                fo.end();
+                let cookie = response.headers['set-cookie'][0];
+                let patc = new RegExp('ASP.NET_SessionId=(.*?);');
+                let mc = patc.exec(cookie);
+                if (mc) {
+                    _ASPNET_SessionId = mc[1];
+                    console.log(`cookie ASP.NET_SessionId: ${_ASPNET_SessionId}`);
+                }
+                let patm =  new RegExp('<input type="hidden" name="_ASPNetRecycleSession" id="_ASPNetRecycleSession" value="(.*?)" />');
+                let mm = patm.exec(html);
+                if (mm) {
+                    _ASPNetRecycleSession = mm[1];
+                    console.log(`_ASPNetRecycleSession: ${_ASPNetRecycleSession}`);
+                }
+                console.log('Step1 done.');
+            } else {
+                let msg = `Step1 HTTP error: ${response.statusMessage}`;
+                console.error(msg);
+            }
+        });
+    }
+
+    let req = http.request("http://twhratsql.whq.wistron/OGWeb/LoginForm.aspx", callback);
+    req.end();
+    req.on('error', e => {
+        let msg = `Step1 Problem: ${e.message}`;
+        console.error(msg);
+    });
+}
+
+step1();
